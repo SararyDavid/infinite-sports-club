fa61c3dc66158526797eea62ecc9a3eb20f6e503
Sarary David <david.sarary123@gmail.com>
2026-04-20 13:25:13 +0700

    fixing the yoga picture

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -32,8 +32,8 @@ const galleryItems = [
   },
   {
     id: 5,
-    src: 'https://stock.adobe.com/search?k=yoga+photos',
-    thumb: 'https://stock.adobe.com/search?k=yoga+photos',
+    src: 'https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=2120&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    thumb: 'https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?q=80&w=2120&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     label: 'Yoga & Recovery',
     span: 'normal',
   },
