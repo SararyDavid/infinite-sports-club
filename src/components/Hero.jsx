26d9b4a1269ecef88d2fe4b68c448c356b4989dc
Sarary David <david.sarary123@gmail.com>
2026-04-23 09:41:22 +0700

    change from train from to trainy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
         
 
         <h1 className="hero__heading fade-in-up" style={{ animationDelay: '0.2s' }}>
-          Train<br />
+          TrainY<br />
           <span className="hero__heading-accent">Without</span><br />
           Limits
         </h1>
